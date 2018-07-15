064bb481c2c6a15f357750db1a057fe0b135f586
nakano57 <hiroto856@gmail.com>
2018-07-15 18:17:14 +0900

    7/11の分

--- kadai.c
+++ kadai.c
@@ -9,28 +9,81 @@ int randm(void){
   return o;
 }
 
+int ban(int men[8][8]){
+  int p,q;
+
+  for(p=0;p<=7;p++){
+    for(q=0;q<=7;q++){
+      if(men[p][q]==1){
+        printf("|○");
+      }else if(men[p][q]==10){
+	printf("|●");
+      }else{
+	printf("| ");
+      }
+      /* //    printf("|%d",men[p][q]); */
+    }
+     printf("|\n");
+
+  }
+}
+
+int canplace(int men[8][8],int inx,int iny){
+  int q;
+
+  if(men[iny-1][inx-1]){
+
+  }else if(men[iny+1][inx-1]){
+
+  }else if(men[iny+1][inx+1]){
+
+  }else if(men[iny-1][inx+1]){
+
+  }else{
+
+  }
+}
 
 
 int main(void){
+
   int men[8][8];
-  int p,q;
+  int p,q,inx,iny;
 
   /*盤面を初期化*/
-  for(p=0;p<=2;p++){
-    for(q=0;q<=2;q++){
+  for(p=0;p<=7;p++){
+    for(q=0;q<=7;q++){
       men[p][q]=0;
-      //printf("|%d",ban[p][q]);
     }
-    // printf("|\n");
   }
+  men[3][3]=1;
+  men[4][4]=1;
+  men[3][4]=10;
+  men[4][3]=10;
 
+  ban(men);
+
+
+ /*先攻後攻を決定*/
   if(randm()==1){
     printf("先攻です\n");
   }else{
     printf("後攻です\n");
+
   }
 
-  while(1){
-  }
+
+
+  //while(1){
+
+  printf("変更箇所を入力してください\n横:");
+  scanf("%d",&inx);
+  printf("縦");
+  scanf("%d",&iny);
+  inx=inx-1;
+  iny=iny-1;
+  men[iny][inx]=1;
+  ban(men);
+  // }
 
 }
