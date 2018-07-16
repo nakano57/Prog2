b00e090deeb25dd95b66f37cd962c7a5aa3669f4
nakano57 <hiroto856@gmail.com>
2018-07-17 07:09:23 +0900

    ひと段落？

--- kadai2.c
+++ kadai2.c
@@ -24,37 +24,32 @@ x = y; y = z; z = w;
 return w = (w ^ (w >> 19)) ^ (t ^ (t >> 8));
 }
 
-/*int cv2bin( unsigned char n ,int DIGITS) {
-    int i,r;
-
-    for( i = DIGITS-1; i >= 0; i-- ) {
-        r=(( n >> i ) & 1);
-        printf("%d",r );
-    }
-    printf( "\n" );
-    return r;
-}*/
-
 
 int main(int argc, char const *argv[]) {
-  int num=atoi(argv[1]),p,q,i,j,bin,r=1;
+  int num=atoi(argv[1]),p,q,i,j,r=1,tmp,zan;
   int w[num][num];
 
 //準備
   for(p=0;p<=num-1;p++){
     for(q=0;q<=num-1;q++){
       w[p][q]=xor128()%num+1;
-      printf("%d", w[p][q]);
+  //    printf("%d", w[p][q]);
     }
-    printf("\n");
+  //  printf("\n");
   }
+  //printf("\n");
 
   for(i=0;i<=num-1;i++){
     r=r*2;
   }
 
   int x[num][r];
-  int res[num][num][r];
+  int result[r];
+
+
+  for(q=0;q<=r-1;q++){
+    result[q]=0;
+  }
 
   for(q=0;q<=r-1;q++){
     for( i = num-1; i >= 0; i-- ) {
@@ -62,21 +57,36 @@ int main(int argc, char const *argv[]) {
         if(x[i][q]==0){
           x[i][q]=-1;
         }
-        printf("%d",x[i][q] );
+        //printf("%d",x[i][q] );
     }
-    printf("\n");
+  //  printf("\n");
+  }
+//  printf("\n");
+
+  for(q=0;q<=r-1;q++){
+    for(i=0;i<=num-1;i++){
+      for(j=0;j<=num-1;j++){
+        result[q]=result[q]+w[i][j]*x[i][q]*x[j][q];
+      }
+    }
+  //  printf("%d\n",result[q] );
   }
 
-for(q=0;q<=r-1;q++){
-  for(i=0;num-1;i++){
-    for(j=0;num-1;j++){
-      res[i][j][q]=w[i][j]*x[i][q]*x[j][q];
-      printf(" %d", res[i][j][q]);
+  for(q=0;q<=r-1;q++){
+    if(q==0){
+        zan=0;
+    }else{
+      if(result[q]<=result[zan]){
+        zan=q;
+      }
     }
   }
-  printf("\n");
-}
 
+  printf("f(x)=%d\nx=(",result[zan]);
+  for(i=0;i<=num-1;i++){
+    printf("%d,",x[i][zan]);
+  }
+  printf(")\n");
 
   return 0;
 }
