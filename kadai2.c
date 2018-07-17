8fe7926ecf53cf5d7319c60c5a28153e67cd820c
nakano57 <hiroto856@gmail.com>
2018-07-17 14:40:36 +0900

    完成！

--- kadai2.c
+++ kadai2.c
@@ -3,10 +3,10 @@
 #include<time.h>
 
 unsigned long long int xor128(void){
-static unsigned int x = 123456789;
-static unsigned int y = 362436069;
-static unsigned int z = 521288629;
-static unsigned int w = 88675123;
+static unsigned long int x = 123456789;
+static unsigned long int y = 362436069;
+static unsigned long int z = 528388629;
+static unsigned long int w = 88675123;
 long unsigned int t;
 
 t = x ^ (x << 11);
@@ -16,11 +16,19 @@ return w = (w ^ (w >> 19)) ^ (t ^ (t >> 8));
 
 
 int main(int argc, char const *argv[]) {
-  long long int num=atoi(argv[1]),p,q,i,j,tmp,zan;
-  long long unsigned int r=1;
+  long long int i,j,tmp,zan;
+  long long unsigned int r=1,p,q,num=atoi(argv[1]);
   long int w[num][num];
+  int **x;
+  int *result;
+//  long long int x[num][r];
+//  long long int result[r];
 
 //準備
+  for(i=0;i<=num-1;i++){
+    r=r*2;
+  }
+
   for(p=0;p<=num-1;p++){
     for(q=0;q<=num-1;q++){
       w[p][q]=xor128()%num+1;
@@ -30,18 +38,23 @@ int main(int argc, char const *argv[]) {
   }
 //  printf("\n");
 
-  for(i=0;i<=num-1;i++){
-    r=r*2;
-  }
 
-  long long int x[num][r];
-  long long int result[r];
+  printf("passed3\n");
+  //long long int result[r];
+  result = (int *)calloc(r, sizeof(int));
+  printf("passed4\n");
+
+  x = (int**)calloc(num, sizeof(int));
+	for (i=0; i<num; i++) {
+		x[i] = (int*)calloc(r, sizeof(int));
+	}
+//  long long int x[num][r];
 
 
-  for(q=0;q<=r-1;q++){
-    result[q]=0;
-  }
-
+//  for(q=0;q<=r;q++){
+//    result[q]=0;
+//  }
+  printf("passed2\n");
   for(q=0;q<=r-1;q++){
     for( i = num-1; i >= 0; i-- ) {
         x[i][q]=(( q >> i ) & 1);
@@ -49,7 +62,7 @@ int main(int argc, char const *argv[]) {
         if(x[i][q]==0){
           x[i][q]=-1;
         }
-        printf("%lld",x[i][q] );
+        printf("%d",x[i][q] );
     }
     printf("\n");
   }
@@ -74,11 +87,14 @@ int main(int argc, char const *argv[]) {
     }
   }
 
-  printf("f(x)=%lld\nx=(",result[zan]);
+  printf("f(x)=%d\nx=(",result[zan]);
   for(i=0;i<=num-1;i++){
-    printf("%lld,",x[i][zan]);
+    printf("%d,",x[i][zan]);
   }
   printf(")\n");
 
+  free(x[0]);
+  free(x);
+  free(result);
   return 0;
 }
